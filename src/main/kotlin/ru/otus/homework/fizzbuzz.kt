ebdf6bbc30b48fa0450894c1a783018135b9b7e1
LexAndro3723 <glebkabackov9@gmail.com>
2023-04-24 14:26:50 +0300

    HomeWork_01

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,5 +1,26 @@
 package ru.otus.homework
 
-
 fun fizzbuzz(n: Int): Array<String> {
+    val Array = Array(n, {0})
+    val Array2 = Array(n, {""})
+
+    for(n1 in 0..n-1){
+        Array[n1] = n1
+    }
+
+    var i = 0
+    for  (fb in Array) {
+        if  (fb == 0 || fb % 3 == 0 && fb % 5 == 0) {
+            Array2[i] = "FizzBuzz"
+        }else if (fb % 3 == 0){
+            Array2[i] = "Fizz"
+        } else if (fb % 5 == 0){
+            Array2[i] = "Buzz"
+        }
+        else {
+            Array2[i] = fb.toString()
+        }
+        i++;
+    }
+    return Array2
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,4 +2,44 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+
+    val array: IntArray  = intArrayOf(0,0)
+
+    var sum = 0
+    var res = false
+    var i = 0
+    var isContains = false
+    for  (fb in  numbers) {
+        if  (sum == 0) {
+            if  (fb < target) {
+                //var isContains = numbers.contains(target-fb)
+                for (k in i + 1..numbers.size - 1) {
+                    if (target - fb  == numbers[k]) {
+                        isContains = true
+                        break
+                    }
+                }
+                if  (isContains) {
+                    array[0] = i
+                    sum = fb
+                }
+            }
+        }
+        else {
+            if  (sum + fb == target) {
+                array[1] = i
+                res = true
+                break
+
+            }
+
+        }
+        i++;
+    }
+    if  (res) {
+        return array
+    }
+    else {
+        throw IllegalArgumentException()
+    }
 }
