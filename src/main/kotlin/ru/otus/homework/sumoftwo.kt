767d512939d8619fa20ce362d661b868ec0370f4
LexAndro3723 <glebkabackov9@gmail.com>
2023-04-26 13:45:36 +0300

    HomeWork_01

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -3,43 +3,19 @@ package ru.otus.homework
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
 
-    val array: IntArray  = intArrayOf(0,0)
-
-    var sum = 0
-    var res = false
     var i = 0
-    var isContains = false
-    for  (fb in  numbers) {
-        if  (sum == 0) {
-            if  (fb < target) {
-                //var isContains = numbers.contains(target-fb)
-                for (k in i + 1..numbers.size - 1) {
-                    if (target - fb  == numbers[k]) {
-                        isContains = true
-                        break
-                    }
-                }
-                if  (isContains) {
-                    array[0] = i
-                    sum = fb
+
+    for (fb in numbers) {
+        if (fb < target) {
+            for (k in i + 1..numbers.size - 1) {
+                if (target - fb == numbers[k]) {
+                    return intArrayOf(i, k)
                 }
             }
         }
-        else {
-            if  (sum + fb == target) {
-                array[1] = i
-                res = true
-                break
 
-            }
+        i++
+    }
+    throw IllegalArgumentException()
 
-        }
-        i++;
-    }
-    if  (res) {
-        return array
-    }
-    else {
-        throw IllegalArgumentException()
-    }
 }
